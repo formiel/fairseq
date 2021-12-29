2e5d50be44a4678fbcda0bfca66678c6e03aa0d0
Hang Le <hangtp.le@gmail.com>
2021-12-29 16:32:12 +0100

    Update st2t fixes and fix load roberta weights in siamese st

--- examples/speech_text_joint_to_text/models/s2t_dualinputtransformer.py
+++ examples/speech_text_joint_to_text/models/s2t_dualinputtransformer.py
@@ -265,9 +265,9 @@ class DualInputEncoder(FairseqEncoder):
                 spch_encoder.transformer_layers[-args.encoder_shared_layers :]
             ):
                 ly_id = i + args.text_encoder_layers - args.encoder_shared_layers
-                # logging.info(f"text_encoder.layers[ly_id]: {type(text_encoder.layers[ly_id])}")
-                # logging.info(f"type(ly): {type(ly)}")
-                # assert isinstance(text_encoder.layers[ly_id], type(ly))
+                if not isinstance(text_encoder.layers[ly_id], type(ly)):
+                    if text_encoder.layers[ly_id]._get_name() not in ('TransformerEncoderLayerBase', 'TransformerEncoderLayer'):
+                        raise ValueError("The shared layers are expected from the same class")
                 text_encoder.layers[ly_id] = cls.set_shared_layer(
                     args.encoder_shared_layer_level,
                     text_encoder.layers[ly_id],
@@ -884,6 +884,7 @@ class DualInputS2TTransformerModel(FairseqEncoderDecoderModel):
             "adaptive_softmax_cutoff": args.adaptive_softmax_cutoff,
             "tie_adaptive_weights": args.tie_adaptive_weights,
             "no_token_positional_embeddings": args.no_token_positional_embeddings,
+            "encoder": {"embed_dim":args.encoder_embed_dim}
         }
         dec_cfg = namedtuple("args", dec_cfg.keys())(*dec_cfg.values())
         dec_emb = nn.Embedding(
