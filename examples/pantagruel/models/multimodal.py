48fa9767575e945f81664172f47e4e3f9aa96bf8
Hang Le <hangtp.le@gmail.com>
2024-10-16 11:27:48 +0700

    add more init scheme

--- examples/pantagruel/models/multimodal.py
+++ examples/pantagruel/models/multimodal.py
@@ -176,6 +176,7 @@ class PantagruelData2VecMultiConfig(FairseqDataclass):
     modality_expert_rank: int = 0
     mlp_after_local_encoder: bool = False
     freeze_project_features: bool = False
+    init_v2: bool = False
 
 
 class LinearDiscriminator(nn.Module):
@@ -358,6 +359,8 @@ class PantagruelMultiModel(BaseFairseqModel):
 
         if self.cfg.mae_init:
             self.apply(self._init_weights)
+        elif self.cfg.init_v2:
+            self.apply(self._init_weights_v2)
         else:
             from fairseq.modules.transformer_sentence_encoder import init_bert_params
 
@@ -418,6 +421,21 @@ class PantagruelMultiModel(BaseFairseqModel):
                 nn.init.constant_(m.bias, 0)
             if m.weight is not None:
                 nn.init.constant_(m.weight, 1.0)
+
+    def _init_weights_v2(self, m):
+
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, mean=0, std=m.weight.shape[1] ** -0.5)
+            # torch.nn.init.kaiming_normal_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+            if m.weight is not None:
+                nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv1d):
+            nn.init.kaiming_normal_(m.weight)
 
     @torch.no_grad()
     def make_ema_teacher(self, ema_decay):
