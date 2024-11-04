cc84c12d69f63ea6ab0cd93dd52e5c1a6c21820a
Hang Le <hangtp.le@gmail.com>
2024-11-04 11:32:59 +0700

    add options to learn tokenizer for huggingface's datasets

--- examples/pantagruel/scripts/data/learn_hf_bpe_tokenizers.py
+++ examples/pantagruel/scripts/data/learn_hf_bpe_tokenizers.py
@@ -1,6 +1,10 @@
 import argparse
 import glob
 import os
+from pathlib import Path
+import psutil
+
+from datasets import load_dataset, Dataset
 from tokenizers import Tokenizer, decoders, models, normalizers, pre_tokenizers, trainers, Regex
 from transformers import PreTrainedTokenizerFast
 
@@ -26,6 +30,13 @@ def main():
     parser.add_argument(
         "--data-dir",
         type=str,
+        default=None,
+        help="Path to the output directory, where the files will be saved",
+    )
+    parser.add_argument(
+        "--arrow-cache-dir",
+        default=None,
+        type=str,
         help="Path to the output directory, where the files will be saved",
     )
     parser.add_argument(
@@ -45,13 +56,43 @@ def main():
     parser.add_argument("--add_prefix_space", action="store_true")
     args = parser.parse_args()
 
-    files = glob.glob(f"{args.data_dir}/*.txt")
-    if not files:
-        print(f"File does not exist: {files}")
-        exit(1)
+    text_files, filtered_dataset = None, None
+    if args.data_dir is not None:
+        assert args.arrow_cache_dir is None
+        text_files = glob.glob(f"{args.data_dir}/*.txt")
+        if not text_files:
+            print(f"File does not exist: {text_files}")
+            exit(1)
+    elif args.arrow_cache_dir is not None:
+        filtered_dir = Path(Path(args.arrow_cache_dir) / "filtered_dataset")
+        if not filtered_dir.exists():
+            mem_before = psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)
+            dataset = load_dataset("manu/french-30b", data_dir="data", split="train", 
+                            cache_dir=args.cache_dir)
+            mem_after = psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)
+            print(f"RAM memory used: {(mem_after - mem_before)} MB")
+            print(f"dataset: {dataset}")
+
+            # filter out parallel en-fr datasets
+            filtered_dataset = dataset.filter(lambda x: x['dataset_id'] != "WmtEnFr" and x['dataset_id'] != "EnglishFrenchWebpagesScrapedTranslated", num_proc=os.cpu_count())
+            print(f"filtered_dataset: {filtered_dataset}")
+            print(f"Saving to local directory at {filtered_dir}")
+            filtered_dataset.save_to_disk(filtered_dir)
+        else:
+            print(f"Loading filtered dataset from {filtered_dir}")
+            filtered_dataset = Dataset.load_from_disk(filtered_dir)
+            print(f"filtered_dataset: {filtered_dataset}")
+        for i in range(0, len(filtered_dataset), 5000000):
+            print(f'{filtered_dataset[i]}')
+    else:
+        raise ValueError("Error: Please supply either --data-dir or --arrow-cache-dir arguments.")
 
     print(f'*** add_prefix_space: {args.add_prefix_space}')
     # print(f'*** unicode_normalizer: {args.unicode_normalizer}')
+
+    def batch_iterator(batch_size=1000):
+        for i in range(0, len(filtered_dataset), batch_size):
+            yield filtered_dataset[i : i + batch_size]["text"]
 
     # Instantiate tokenizer
     tokenizer = Tokenizer(models.BPE(
@@ -81,8 +122,15 @@ def main():
                                         limit_alphabet=10000,
                                         initial_alphabet=[],
                                         )
-    
-    tokenizer.train(files, trainer=bpe_trainer)
+    if text_files is not None:
+        tokenizer.train(text_files, trainer=bpe_trainer)
+    elif filtered_dataset is not None:
+        it = batch_iterator()
+        tokenizer.train_from_iterator(it,
+                                  trainer=bpe_trainer,
+                                  length=len(filtered_dataset))
+    else:
+        raise NotImplementedError("Train on text files or HF's datasets")
 
     # Save the files
     os.makedirs(args.out_dir, exist_ok=True)
