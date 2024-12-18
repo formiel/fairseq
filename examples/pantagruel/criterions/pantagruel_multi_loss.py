fix bugs in ctc loss and wer computation

examples/pantagruel/criterions/pantagruel_multi_loss.py
@@ -168,13 +168,18 @@ class PantagruelMultiCriterion(FairseqCriterion):
 
         ctc_loss, lprobs = None, None
         if self.ctc_weight > 0 and net_output["ctc_out"]:
-            ctc_loss, lprobs, sample_size_ctc = self.compute_ctc_loss(net_output["ctc_out"], net_input)
+            ctc_loss, lprobs, sample_size_ctc, input_lengths = self.compute_ctc_loss(
+                net_output["ctc_out"], net_input
+            )
             logging_output["loss_ctc"] = ctc_loss
             logging_output["sample_size_ctc"] = sample_size_ctc
-            loss = loss + ctc_loss
+            if not net_output["ctc_out"]["is_frozen"]:
+                loss = loss + ctc_loss
 
             if not model.training:
-                _ctc_logging = self.compute_wer(lprobs, net_output["ctc_out"], net_input)
+                _ctc_logging = self.compute_wer(
+                    lprobs, net_output["ctc_out"], net_input, input_lengths
+                )
                 for k, v in _ctc_logging.items():
                     logging_output[k] = v
 
@@ -189,10 +194,19 @@ class PantagruelMultiCriterion(FairseqCriterion):
     def compute_ctc_loss(self, ctc_out, net_input):
         lprobs = utils.log_softmax(ctc_out["x"], dim=-1).contiguous()  # (T, B, C) from the encoder
 
-        src_text = net_input["source"]["text"]["source"]
-        if isinstance(ctc_out["padding_mask"], torch.Tensor):
-            non_padding_mask = ~ctc_out["padding_mask"]
-            input_lengths = non_padding_mask.long().sum(-1) + 1
+        src_text = net_input["source"]["text"]["source"] # no special bos and eos tokens here
+        encoder_padding_mask = ctc_out["padding_mask"]
+        if isinstance(encoder_padding_mask, torch.Tensor) and encoder_padding_mask.any():
+            if lprobs.size(0) > encoder_padding_mask.size(1):
+                encoder_padding_mask = F.pad(
+                    encoder_padding_mask, (1, 0), value=False
+                )
+            non_padding_mask = ~encoder_padding_mask
+            offset = 0
+            input_lengths = non_padding_mask.long().sum(-1)
+            if torch.max(input_lengths) < lprobs.size(0):
+                offset = 1
+            input_lengths = input_lengths + offset
         else:
             input_lengths = lprobs.new_full(
                 (lprobs.size(1),), lprobs.size(0), dtype=torch.long
@@ -214,22 +228,13 @@ class PantagruelMultiCriterion(FairseqCriterion):
             )
 
         # ntokens = target_lengths.sum().item()
-        # sample_size_ctc = src_text.size(0)
-        sample_size_ctc = target_lengths.sum().item()
+        sample_size_ctc = src_text.size(0)
 
-        return ctc_loss, lprobs, sample_size_ctc
+        return ctc_loss, lprobs, sample_size_ctc, input_lengths
 
-    def compute_wer(self, lprobs, ctc_out, net_input):
+    def compute_wer(self, lprobs, ctc_out, net_input, input_lengths):
 
         src_text = net_input["source"]["text"]["source"]
-        if isinstance(ctc_out["padding_mask"], torch.Tensor):
-            non_padding_mask = ~ctc_out["padding_mask"]
-            input_lengths = non_padding_mask.long().sum(-1) + 1
-        else:
-            input_lengths = lprobs.new_full(
-                (lprobs.size(1),), lprobs.size(0), dtype=torch.long
-            )
-
         target_lengths = net_input["source"]["text"]["src_txt_lengths"]
 
         _logging_output = {}
@@ -275,7 +280,7 @@ class PantagruelMultiCriterion(FairseqCriterion):
 
                 w_len += len(targ_words)
 
-            # printing out results for checking
+            # printing out decoding results
             logger.info(f"[TGT]: {' '.join(targ_words)}")
             logger.info(f"[HYP]: {' '.join(pred_words_raw)}")
 

examples/pantagruel/models/multimodal.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import contextlib
 import logging
 import math
 from dataclasses import dataclass, field
@@ -10,7 +11,7 @@ from typing import Optional, Callable
 from functools import partial
 import numpy as np
 
-from omegaconf import II
+from omegaconf import II, MISSING
 
 import torch
 import torch.nn as nn
@@ -185,6 +186,7 @@ class PantagruelData2VecMultiConfig(FairseqDataclass):
     do_shallow_fusion: bool = True
     text_scale_in_fusion: float = 1.0
     use_ctc_module: bool = False
+    num_freeze_ctc_updates: int = 0
 
 
 class CTCDecoder(nn.Module):
@@ -295,6 +297,7 @@ class PantagruelMultiModel(BaseFairseqModel):
         self.do_shallow_fusion = getattr(cfg, "do_shallow_fusion", False)
         self.text_scale_in_fusion = getattr(cfg, "text_scale_in_fusion", 1.0)
         self.use_ctc_module = getattr(cfg, "use_ctc_module", False)
+        self.num_freeze_ctc_updates = getattr(cfg, "num_freeze_ctc_updates", 0)
 
         make_layer_norm = partial(
             nn.LayerNorm, eps=cfg.norm_eps, elementwise_affine=cfg.norm_affine
@@ -325,8 +328,9 @@ class PantagruelMultiModel(BaseFairseqModel):
             )
 
         token_type_embeddings = None
+        self.single_modalities = [m for m in self.modalities if "_" not in m.name]
         if cfg.use_token_type_embeddings:
-            token_type_embeddings = nn.Embedding(len(self.modalities), cfg.embed_dim)
+            token_type_embeddings = nn.Embedding(len(self.single_modalities), cfg.embed_dim)
             # nn.init.kaiming_normal_(token_type_embeddings.weight)
 
         self.alibi_biases = {}
@@ -672,7 +676,7 @@ class PantagruelMultiModel(BaseFairseqModel):
         token_type_ids = None
         remaining_token_type_ids = {}
 
-        for it, im in enumerate(self.modalities):
+        for it, im in enumerate(self.single_modalities):
             if im.name == mode:
                 token_type_ids = torch.ones((1), dtype=torch.int64, device=device) * it
             else:
@@ -815,14 +819,20 @@ class PantagruelMultiModel(BaseFairseqModel):
 
         ctc_out = {}
         if self.ctc_module is not None and mode == "AUDIO_TEXT":
-            _speech_enc_out = feature_extractor["AUDIO"].contextualized_features(
-                    extractor_out["audio"]["local_features"],
-                    source[_mod]["padding_mask"],
-                    mask=False,
-                    remove_masked=False,
-                ) # BxLxD
-            ctc_out["x"] = self.ctc_module(_speech_enc_out["x"])
-            ctc_out["padding_mask"] = _speech_enc_out["padding_mask"]
+            ft = self.num_freeze_ctc_updates <= self.num_updates
+            with torch.no_grad() if not ft else contextlib.ExitStack():
+                _speech_enc_out = feature_extractor["AUDIO"].contextualized_features(
+                        extractor_out["audio"]["local_features"],
+                        source[_mod]["padding_mask"],
+                        mask=False,
+                        remove_masked=False,
+                    ) # BxLxD
+                ctc_out["x"] = self.ctc_module(_speech_enc_out["x"])
+                ctc_out["padding_mask"] = _speech_enc_out["padding_mask"]
+                if ft:
+                    ctc_out["is_frozen"] = False
+                else:
+                    ctc_out["is_frozen"] = True
 
         if features_only:
             if remove_extra_tokens: