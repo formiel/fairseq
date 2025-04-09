cf65b60aa17c0c2589f5071492fe2cb41c4d3f7d
Hang Le <hangtp.le@gmail.com>
2025-04-09 10:33:27 +0200

    fix small bug

--- examples/pantagruel/models/pantagruel.py
+++ examples/pantagruel/models/pantagruel.py
@@ -429,7 +429,7 @@ class PantagruelMultiModel(BaseFairseqModel):
             if skip:
                 skip = eval(skip)
                 for k, v in skip.items():
-                    if v.lower() == "none":
+                    if v[0].lower() == "none":
                         skip[k] = []
             return skip
 
