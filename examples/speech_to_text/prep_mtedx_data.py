b05104741d70271b3d2392dc0bc12909c82b5f53
Hang Le <hangtp.le@gmail.com>
2025-06-26 14:17:25 +0200

    fix bug in processing mtedx

--- examples/speech_to_text/prep_mtedx_data.py
+++ examples/speech_to_text/prep_mtedx_data.py
@@ -127,7 +127,7 @@ def process(args):
             dataset = mTEDx(root.as_posix(), lang, split)
             if args.use_audio_input:
                 print("Converting audios...")
-                for waveform, sample_rate, _, _, _, utt_id in tqdm(dataset):
+                for waveform, sample_rate, _, _, _, _, utt_id in tqdm(dataset):
                     tgt_sample_rate = 16_000
                     _wavform, _ = convert_waveform(
                         waveform, sample_rate, to_mono=True,
@@ -135,7 +135,7 @@ def process(args):
                     )
                     sf.write(
                         (audio_root / f"{utt_id}.flac").as_posix(),
-                        _wavform.numpy(), tgt_sample_rate
+                        _wavform.squeeze().cpu().numpy(), tgt_sample_rate
                     )
             else:
                 print("Extracting log mel filter bank features...")
